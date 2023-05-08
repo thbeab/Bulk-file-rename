acb79fc52ebdd4bb84838d91261b383aabb29660
Thomas Beaugendre <thomas-frederic.beaugendre@polymtl.ca>
2023-05-08 11:02:41 -0400

    added renaming in new thread

--- app/rename.py
+++ app/rename.py
@@ -1,10 +1,32 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# rename.py
+# rprename/rename.py
 
-"""This module provides the RP Renamer entry-point script."""
+"""This module provides the Renamer class to rename multiple files."""
 
-from app.app import main
+import time
+from pathlib import Path
 
-if __name__ == "__main__":
-    main()
+from PyQt6.QtCore import QObject, pyqtSignal
+
+class Renamer(QObject):
+    # Define custom signals
+    progressed = pyqtSignal(int)
+    renamedFile = pyqtSignal(Path)
+    finished = pyqtSignal()
+
+    def __init__(self, files, prefix):
+        super().__init__()
+        self._files = files
+        self._prefix = prefix
+
+    def renameFiles(self):
+        for fileNumber, file in enumerate(self._files, 1):
+            newFile = file.parent.joinpath(
+                f"{self._prefix}{str(fileNumber)}{file.suffix}"
+            )
+            file.rename(newFile)
+            time.sleep(0.1)  # Comment this line to rename files faster.
+            self.progressed.emit(fileNumber)
+            self.renamedFile.emit(newFile)
+        self.progressed.emit(0)  # Reset the progress
+        self.finished.emit()
--- app/views.py
+++ app/views.py
@@ -6,8 +6,10 @@
 from collections import deque
 from pathlib import Path
 
+from PyQt6.QtCore import QThread
 from PyQt6.QtWidgets import QFileDialog, QWidget
 
+from .rename import Renamer
 from .ui.window import Ui_Window
 
 FILTERS = ";;".join(
@@ -34,6 +36,7 @@ class Window(QWidget, Ui_Window):
 
     def _connectSignalsSlots(self):
         self.loadFilesButton.clicked.connect(self.loadFiles)
+        self.renameFilesButton.clicked.connect(self.renameFiles)
 
     def loadFiles(self):
         self.dstFileList.clear()
@@ -53,3 +56,35 @@ class Window(QWidget, Ui_Window):
                 self._files.append(Path(file))
                 self.srcFileList.addItem(file)
             self._filesCount = len(self._files)
+    
+    def renameFiles(self):
+        self._runRenamerThread()
+
+    def _runRenamerThread(self):
+        prefix = self.prefixEdit.text()
+        self._thread = QThread()
+        self._renamer = Renamer(
+            files=tuple(self._files),
+            prefix=prefix,
+        )
+        self._renamer.moveToThread(self._thread)
+        # Rename
+        self._thread.started.connect(self._renamer.renameFiles)
+        # Update state
+        self._renamer.renamedFile.connect(self._updateStateWhenFileRenamed)
+        self._renamer.progressed.connect(self._updateProgressBar)
+        # Clean up
+        self._renamer.finished.connect(self._thread.quit)
+        self._renamer.finished.connect(self._renamer.deleteLater)
+        self._thread.finished.connect(self._thread.deleteLater)
+        # Run the thread
+        self._thread.start()
+
+    def _updateStateWhenFileRenamed(self, newFile):
+        self._files.popleft()
+        self.srcFileList.takeItem(0)
+        self.dstFileList.addItem(str(newFile))
+
+    def _updateProgressBar(self, fileNumber):
+        progressPercent = int(fileNumber / self._filesCount * 100)
+        self.progressBar.setValue(progressPercent)
